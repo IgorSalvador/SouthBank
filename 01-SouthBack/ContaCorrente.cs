51c2b0c2a1d2486c561fc361bb68508af61105fb
Igor Henrique Salvador <igorsalvador0621@gmail.com>
2021-03-26 22:13:41 -0300

    Update ContaCorrente.cs
    Implementando criação de valor para agencia e saldo

--- 01-SouthBack/ContaCorrente.cs
+++ 01-SouthBack/ContaCorrente.cs
@@ -7,5 +7,24 @@ namespace _01_SouthBack
     class ContaCorrente
     {
         public Cliente Titular;
+        public int Agencia { get; set; }
+        public int Conta { get; set; }
+        private double _saldo;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                //Value representa o valor externo a ser adicionado
+                if(value < 0)
+                {
+                    _saldo = value;
+                }
+            }
+        }
+
     }
 }
